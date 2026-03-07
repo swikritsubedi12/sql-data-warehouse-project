fdfdbdf8b511289f954a931f5eb99a476a695b9d
swikritsubedi12 <swikritsubedi12@gmail.com>
2026-03-07 08:24:31 +0545

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,14 @@
+-- Create Database 'DataWarehouse'--
+
+USE master;
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
+
+--Create Schemas--
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
